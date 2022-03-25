a1a998068582ac9d257e37e2ce1bcdfefb81f9f6
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-03-25 16:19:56 +0700

    Create user.model.js

---
+++ server/model/user.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// username unique validator
+const unique = require("mongoose-unique-validator");
+
+// create schema
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+// apply unique validator
+userSchema.plugin(unique);
+
+// for hashing password
+const bcrypt = require("bcrypt");
+
+// hash before using user
+userSchema.pre("save", (next) => {
+  // store self to use in child functions
+  const user = this;
+  // hashing
+  bcrypt.hash(user.password, 10, (err, hashedPassword) => {
+    if (err) return console.log(err);
+    user.password = hashedPassword;
+    next();
+  });
+});
+
+module.exports = mongoose.model("User", userSchema);
