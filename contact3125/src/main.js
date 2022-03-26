7dfa3cde0f297acb180e629e0760584bec513318
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-03-26 18:05:40 +0700

    fix : mispelling

--- contact3125/src/main.js
+++ contact3125/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 // import css
-import FomanticUI from "./vue-fomantic-ui";
+import FomanticUI from "vue-fomantic-ui";
 import "fomantic-ui-css/semantic.min.css";
 import "./index.css";
 
