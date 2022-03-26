0502e948d98ad0eb2e14b830411dd79618d2c4cd
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-03-26 17:36:40 +0700

    usingl CSS framework

--- contact3125/src/main.js
+++ contact3125/src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+
+// import css
+import FomanticUI from "./vue-fomantic-ui";
+import "fomantic-ui-css/semantic.min.css";
 import "./index.css";
 
 // import router
 import router from "./routers";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(FomanticUI).mount("#app");
