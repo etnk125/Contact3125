149af2f4bb2e6bba98a7682f8e04e8ab5ba47cea
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-03-26 12:18:02 +0700

    update controller

--- server/controller/post.controller.js
+++ server/controller/post.controller.js
@@ -1,2 +1,16 @@
 const Contact = require("../model/contact.model");
 
+//let's try to using callback function
+
+module.exports = (req, res) => {
+  Contact.create(req.body, (err, contact) => {
+    if (err) {
+      console.log(`Error to add contact `);
+      console.error(err);
+      return res.redirect("/");
+    } else {
+      res.json(contact);
+    }
+    res.redirect("/");
+  });
+};
