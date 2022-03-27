4dcd5e437a2523519bc9e9039b9af44967b727eb
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-03-27 20:40:38 +0700

    Update ContactCard.vue

--- contact3125/src/components/ContactCard.vue
+++ contact3125/src/components/ContactCard.vue
@@ -1,7 +1,38 @@
 <template>
-  <h1>this is contact card</h1>
+  <sui-card @click="modal=true">
+    <sui-image src="https://nightswinger.github.io/vue-fomantic-ui/images/avatar/large/kristy.png" />
+    <sui-card-content>
+      <sui-card-header>{{contact.firstname}} {{contact.lastname}}</sui-card-header>
+      <sui-card-meta>
+        <p>{{contact.email}}</p>
+        <span>{{contact.mobile}}</span>
+      </sui-card-meta>
+    </sui-card-content>
+  </sui-card>
+  <contact-modal :contact="contact" :modal="modal" :closeModal="()=>(modal=false)" />
 </template>
 
 <script>
-export default {};
+import { onMounted } from "vue";
+import ContactModal from "./ContactModal.vue";
+
+export default {
+  props: {
+    contact: Object,
+  },
+  // setup({ contact }) {
+  //   onMounted(() => {
+  //     console.log(contact);
+  //   });
+  // },
+  components: { ContactModal },
+  data() {
+    return { modal: false };
+  },
+};
 </script>
+<style scoped>
+.content {
+  text-align: center;
+}
+</style>
