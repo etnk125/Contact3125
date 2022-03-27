11ca0b0a6ec08d8ac8d18249071f40c398430f70
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-03-27 20:40:34 +0700

    Create ContactModal.vue

---
+++ contact3125/src/components/ContactModal.vue
@@ -0,0 +1,22 @@
+<template>
+  <sui-modal v-model="modal">
+    <sui-modal-content>{{contact}}</sui-modal-content>
+    <sui-modal-actions>
+      <sui-button positive @click="closeModal()">view profile</sui-button>
+      <sui-button @click="closeModal()">close</sui-button>
+    </sui-modal-actions>
+  </sui-modal>
+</template>
+
+<script>
+export default {
+  props: {
+    modal: false,
+    contact: Object,
+    closeModal: Function,
+  },
+};
+</script>
+
+<style>
+</style>
