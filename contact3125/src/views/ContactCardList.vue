c61160223c300106d685702f9ac6c784a998b649
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-03-27 20:41:17 +0700

    Create ContactCardList.vue

---
+++ contact3125/src/views/ContactCardList.vue
@@ -0,0 +1,36 @@
+<template>
+  <sui-card-group>
+    <ContactCard
+      v-for="contact in contacts.filter((contact) =>
+        contact.firstname.includes(this.query) ||
+        contact.lastname.includes(this.query))"
+      :contact="contact"
+    />
+  </sui-card-group>
+</template>
+
+<script>
+import { onMounted } from "@vue/runtime-core";
+import { computed } from "vue";
+import { getContactList } from "../services/contact.service";
+import ContactCard from "../components/ContactCard.vue";
+
+export default {
+  props: {
+    query: String,
+  },
+  data() {
+    return { contacts: [] };
+  },
+  async created() {
+    this.contacts = await getContactList();
+  },
+  components: { ContactCard },
+};
+</script>
+
+<style scoped>
+.cards {
+  justify-content: center;
+}
+</style>
