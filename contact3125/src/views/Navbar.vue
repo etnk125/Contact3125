7e4d8e7246d16e081d234f312e9165a7a1549c29
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-03-30 19:28:41 +0700

    navbar link

--- contact3125/src/views/Navbar.vue
+++ contact3125/src/views/Navbar.vue
@@ -1,15 +1,21 @@
 <template>
   <sui-menu inverted>
-    <sui-menu-item>
-      <img src="../assets/logo.png" alt="contact page" />
-    </sui-menu-item>
+    <router-link :to="{path:'/'}">
+      <sui-menu-item>
+        <img src="../assets/logo.png" alt="contact page" />
+      </sui-menu-item>
+    </router-link>
 
-    <sui-menu-item>Contact</sui-menu-item>
+    <router-link :to="{name:'Contact'}">
+      <sui-menu-item>Contact</sui-menu-item>
+    </router-link>
 
     <sui-menu-menu position="right">
-      <sui-menu-item id="icon">
-        <sui-icon name="user" />
-      </sui-menu-item>
+      <router-link :to="{name:'Login'}">
+        <sui-menu-item>
+          <sui-icon name="user" />
+        </sui-menu-item>
+      </router-link>
     </sui-menu-menu>
   </sui-menu>
 </template>
