d637ba1d6bdd654a42cc408a5963d83c20c0fb3f
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-03-30 19:27:52 +0700

    Rename path

--- contact3125/src/views/SearchForm.vue
+++ contact3125/src/views/SearchForm.vue
@@ -33,7 +33,7 @@ export default {
   methods: {
     submitHandler() {
       console.log(this.query);
-      this.$router.push({ name: "contact", query: { q: this.query } });
+      this.$router.push({ name: "Contact", query: { q: this.query } });
       // this.query = "";
     },
     async fetchData() {
