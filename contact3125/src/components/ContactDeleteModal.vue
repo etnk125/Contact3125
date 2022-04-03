a2f1b38d7b9bf32a1d6477db0a77cae6ba58285b
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-04-03 18:40:55 +0700

    Create ContactDeleteModal.vue

---
+++ contact3125/src/components/ContactDeleteModal.vue
@@ -0,0 +1,45 @@
+<template>
+  <sui-modal basic v-model="modal">
+    <div class="delete-modal-wrapper">
+      <h2 class="delete-modal-header">
+        <sui-modal-header icon>
+          <sui-icon name="trash alternate" />Delete Contact
+        </sui-modal-header>
+      </h2>
+      <sui-modal-content>
+        <h4>Are you sure to delete contact ID:{{contact.cid}}</h4>
+      </sui-modal-content>
+      <sui-modal-actions>
+        <sui-button color="red" inverted @click="deleteHandler">
+          <sui-icon name="remove" />Delete
+        </sui-button>
+        <sui-button basic color="white" inverted @click="closeModal">
+          <sui-icon name="checkmark" />Close
+        </sui-button>
+      </sui-modal-actions>
+    </div>
+  </sui-modal>
+</template>
+
+<script>
+export default {
+  props: {
+    modal: false,
+    contact: Object,
+    closeModal: Function,
+    deleteHandler: Function,
+  },
+};
+</script>
+
+<style scoped>
+.delete-modal-wrapper {
+  text-align: center;
+}
+.delete-modal-wrapper > .header {
+  align-items: center;
+}
+.delete-modal-wrapper > * {
+  margin-top: 30px;
+}
+</style>
