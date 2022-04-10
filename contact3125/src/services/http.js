68da8665aac995d24e031b05e6515d6e7e216cd5
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-04-11 00:43:04 +0700

    using production backend url

--- contact3125/src/services/http.js
+++ contact3125/src/services/http.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 // work in local
-const URL = "http://localhost:5000";
+// const URL = "http://localhost:5000";
+// deployment
+const URL = "https://contact3125.herokuapp.com/";
 
 const httpClient = axios.create({
   baseURL: URL,
