9a3c3e5836eb865962a177b030aa2ce692c9890f
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-04-10 21:36:47 +0700

    Update http.js

--- contact3125/src/services/http.js
+++ contact3125/src/services/http.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 // work in local
-const URL = "http://localhost:5000";
+// const URL = "http://localhost:5000";
+// for deployment
+const URL = "https://contact3125-node.herokuapp.com/";
 
 const httpClient = axios.create({
   baseURL: URL,
