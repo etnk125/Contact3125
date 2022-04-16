610cc16c1b714de8a1ef029dd13366424f49de73
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-04-17 03:30:18 +0700

    facebook button

--- contact3125/src/components/ContactModal.vue
+++ contact3125/src/components/ContactModal.vue
@@ -12,7 +12,9 @@
     </sui-modal-content>
 
     <sui-modal-actions>
-      <sui-button positive @click="closeModal()">view profile</sui-button>
+      <sui-button positive v-if="contact.facebook!=''">
+        <a :href="contact.facebook" target="_blank">view profile</a>
+      </sui-button>
       <sui-button @click="closeModal()">close</sui-button>
     </sui-modal-actions>
   </sui-modal>
@@ -28,5 +30,9 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+a {
+  text-decoration: none;
+  color: white;
+}
 </style>
