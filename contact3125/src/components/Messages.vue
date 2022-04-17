52a8bc46c8f57dcfef01cf1d1568d450ec9a093f
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-04-17 13:14:37 +0700

    implement message

---
+++ contact3125/src/components/Messages.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-for="message in errMessages">
+    <TheMessage :message="message.message" :ID="message.ID" />
+  </div>
+</template>
+
+<script>
+import store from "../store";
+import TheMessage from "./TheMessage.vue";
+
+export default {
+  computed: {
+    errMessages() {
+      return store.errMessages;
+    },
+  },
+  components: {
+    TheMessage,
+  },
+};
+</script>
+
+<style>
+</style>
