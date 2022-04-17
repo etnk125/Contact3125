29cc499483aa5a40678811733bae65d2d9a0eeb7
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-04-17 13:14:09 +0700

    error message controller

---
+++ contact3125/src/store/index.js
@@ -0,0 +1,20 @@
+import { reactive } from "vue";
+
+const store = reactive({
+  currentID: 0,
+  errMessages: [{ ID: 0, message: "this is err" }],
+  addMessage(message = "something went wrong") {
+    this.errMessages = [...this.errMessages, { message, ID: ++this.currentID }];
+  },
+  removeMessage(ID) {
+    this.errMessages = this.errMessages.filter((message) => message.ID != ID);
+    // this.errMessages = [
+    //   ...this.errMessages.filter((message) => message.ID != ID),
+    // ];
+  },
+  clearMessage() {
+    this.errMessages = [];
+  },
+});
+
+export default store;
